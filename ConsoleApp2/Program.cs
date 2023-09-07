9a5943f8e9a8abdabd06cb3c0744d062e26e1b4d
Vsevolod <Vsevolod@LAPTOP-2F90RG0P>
2023-09-07 18:39:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class EvenNumber
+{
+    private int value;
+
+    public EvenNumber(int value)
+    {
+        this.value = value;
+    }
+
+    public static bool operator true(EvenNumber even)
+    {
+        return even.value % 2 == 0;
+    }
+
+    public static bool operator false(EvenNumber even)
+    {
+        return even.value % 2 != 0;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        EvenNumber even1 = new EvenNumber(4);
+        EvenNumber even2 = new EvenNumber(7);
+
+        if (even1)
+            Console.WriteLine("Четное число");
+        else
+            Console.WriteLine("Нечетное число");
+
+        if (even2)
+            Console.WriteLine("Четное число");
+        else
+            Console.WriteLine("Нечетное число");
+    }
+}
